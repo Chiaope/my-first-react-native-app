9acc757a7e220bc8917d01add8bac817ade57356
gui kang <gui_kang@hotmail.com>
2024-06-23 22:54:35 +0800

    Created basic elements for to do list

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.titleContainter}>
+        <Text style={styles.titleTextStyle}>To Do List</Text>
+      </View>
+      <View style={styles.taskContainer}>
+        < TextInput style={styles.inputStyle} placeholder='Give me tasks'/>
+        <Button title='Tap me!!' />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  appContainer: {
+    marginVertical: '15%',
+    margin: '5%'
+  },
+  titleContainter: {
+    alignItems: 'center',
+    padding: '5%'
+  },
+  titleTextStyle: {
+    fontSize: 30
+  },
+  taskContainer: {
+    // borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '1%'
+  },
+  inputStyle: {
+    borderWidth: 1,
+    width: '80%',
+    paddingHorizontal: '2%'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
